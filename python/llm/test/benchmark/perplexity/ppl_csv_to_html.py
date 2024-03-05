e9dbf2976bd3a028ac700ee36c2332026ff95d56
Huang, Xinshengzi <xinshengzi.huang@intel.com>
2024-03-05 14:23:28 +0800

    update format of html

--- python/llm/test/benchmark/perplexity/ppl_csv_to_html.py
+++ python/llm/test/benchmark/perplexity/ppl_csv_to_html.py
@@ -37,8 +37,8 @@ def nonzero_min(lst):
     non_zero_lst = [num for num in lst if num > 0.0]
     return min(non_zero_lst) if non_zero_lst else None
 
-def is_diffs_within_normal_range(diff_en, diff_zh, threshold=5.0):
-    return not any(diff < (-threshold) for diff in diff_en + diff_zh if isinstance(diff, float))
+def is_diffs_within_normal_range(diff_ppl, threshold=5.0):
+    return not any(diff < (-threshold) for diff in diff_ppl if isinstance(diff, float))
 
 def add_to_dict(dict, key, value):
     if key not in dict:
@@ -60,8 +60,7 @@ def create_fp16_dict(fp16_path):
         model = row['Model']
         # Formalize the data to have 2 decimal places
         fp16_dict[model] = {
-            'en': "{:.2f}".format(row['en']),
-            'zh': "{:.2f}".format(row['zh'])
+            'ppl': "{:.2f}".format(row['ppl'])
         }
     return fp16_dict
 
@@ -105,9 +104,8 @@ def main():
     diffs_within_normal_range = True
 
     # Add display of FP16 values for each model and add percentage difference column
-    for task in ['en', 'zh']:
-        latest_csv[f'{task}_FP16'] = latest_csv['Model'].apply(lambda model: fp16_dict.get(model, {}).get(task, 'N/A'))
-        latest_csv[f'{task}_diff_FP16(%)'] = latest_csv.apply(lambda row: calculate_percentage_difference(row[task], row[f'{task}_FP16']), axis=1)
+    latest_csv[f'FP16'] = latest_csv['Model'].apply(lambda model: fp16_dict.get(model, {}).get('ppl', 'N/A'))
+    latest_csv[f'diff_FP16(%)'] = latest_csv.apply(lambda row: calculate_percentage_difference(row['ppl'], row[f'FP16']), axis=1)
 
     if len(csv_files)>1:
         if args.baseline_path:
@@ -115,13 +113,8 @@ def main():
         else:
             previous_csv = pd.read_csv(csv_files[1], index_col=0)
 
-        last_en=['']*len(latest_csv.index)
-        diff_en=['']*len(latest_csv.index)
-        last_zh=['']*len(latest_csv.index)
-        diff_zh=['']*len(latest_csv.index)
-
-        en='en'
-        zh='zh'
+        last_ppl=['']*len(latest_csv.index)
+        diff_ppl=['']*len(latest_csv.index)
 
         csv_dict = {}
         for csv_file in csv_files:
@@ -129,17 +122,14 @@ def main():
             for current_csv_ind,current_csv_row in current_csv.iterrows():
                 current_csv_model=current_csv_row['Model'].strip()
                 current_csv_precision=current_csv_row['Precision'].strip()
-                current_csv_model_en=current_csv_model+'-'+current_csv_precision+'-'+'en'
-                current_csv_model_zh=current_csv_model+'-'+current_csv_precision+'-'+'zh'
-                add_to_dict(csv_dict, current_csv_model_en, current_csv_row[en])
-                add_to_dict(csv_dict, current_csv_model_zh, current_csv_row[zh]) 
+                current_csv_model_ppl=current_csv_model+'-'+current_csv_precision+'-'+'ppl'
+                add_to_dict(csv_dict, current_csv_model_ppl, current_csv_row['ppl'])
 
         for latest_csv_ind,latest_csv_row in latest_csv.iterrows():
 
             latest_csv_model=latest_csv_row['Model'].strip()
             latest_csv_precision=latest_csv_row['Precision'].strip()
-            latest_en=latest_csv_row[en]
-            latest_zh=latest_csv_row[zh]
+            latest_ppl=latest_csv_row['ppl']
 
             in_previous_flag=False
 
@@ -150,39 +140,30 @@ def main():
 
                 if latest_csv_model==previous_csv_model and latest_csv_precision==previous_csv_precision:
 
-                    previous_en=previous_csv_row[en]
-                    previous_zh=previous_csv_row[zh]
-                    if previous_en > 0.0 or previous_zh > 0.0:
-                        last_en[latest_csv_ind]=previous_en
-                        diff_en[latest_csv_ind]=round((latest_en-previous_en)*100/previous_en,2)
-                        last_zh[latest_csv_ind]=previous_zh
-                        diff_zh[latest_csv_ind]=round((latest_zh-previous_zh)*100/previous_zh,2)
+                    previous_ppl=previous_csv_row['ppl']
+                    if previous_ppl > 0.0:
+                        last_ppl[latest_csv_ind]=previous_ppl
+                        diff_ppl[latest_csv_ind]=round((latest_ppl-previous_ppl)*100/previous_ppl,2)
                         in_previous_flag=True
 
             if not in_previous_flag:
-                last_en[latest_csv_ind]=pd.NA
-                diff_en[latest_csv_ind]=pd.NA
-                last_zh[latest_csv_ind]=pd.NA
-                diff_zh[latest_csv_ind]=pd.NA
+                last_ppl[latest_csv_ind]=pd.NA
+                diff_ppl[latest_csv_ind]=pd.NA
 
-        latest_csv.insert(loc=9,column='last_en',value=last_en)
-        latest_csv.insert(loc=10,column='diff_en(%)',value=diff_en)
-        latest_csv.insert(loc=11,column='last_zh',value=last_zh)
-        latest_csv.insert(loc=12,column='diff_zh(%)',value=diff_zh)
+        latest_csv.insert(loc=9,column='last_ppl',value=last_ppl)
+        latest_csv.insert(loc=10,column='diff_ppl(%)',value=diff_ppl)
 
 
-        diffs_within_normal_range = is_diffs_within_normal_range(diff_en, diff_zh, threshold=highlight_threshold)
+        diffs_within_normal_range = is_diffs_within_normal_range(diff_ppl, threshold=highlight_threshold)
 
-        subset1=['diff_en(%)','diff_zh(%)']
+        subset1=['diff_ppl(%)']
         
-        columns={'en': '{:.2f}', 'zh': '{:.2f}', 'last_en': '{:.2f}', 'diff_en(%)': '{:.2f}',
-                'last_zh': '{:.2f}', 'diff_zh(%)': '{:.2f}'}
+        columns={'ppl': '{:.2f}', 'last_ppl': '{:.2f}', 'diff_ppl(%)': '{:.2f}'}
 
         latest_csv.drop('Index', axis=1, inplace=True)
 
         styled_df = latest_csv.style.format(columns).applymap(lambda val: highlight_vals(val, max=3.0, is_last=True), subset=subset1)
-        for task in ['en', 'zh']:
-            styled_df = styled_df.applymap(lambda val: highlight_vals(val, max=highlight_threshold, is_last=False), subset=[f'{task}_diff_FP16(%)'])
+        styled_df = styled_df.applymap(lambda val: highlight_vals(val, max=highlight_threshold, is_last=False), subset=[f'diff_FP16(%)'])
         
         # add css style to restrict width and wrap text
         styled_df.set_table_styles([{
